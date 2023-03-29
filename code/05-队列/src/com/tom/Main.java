3baccd36f0da13808196b652504634594cd842b2
tomtao626 <tp320670258@gmail.com>
2023-03-30 03:00:26 +0800

    add 04-Queue-Qeque

--- code/05-队列/src/com/tom/Main.java
+++ code/05-队列/src/com/tom/Main.java
@@ -2,15 +2,26 @@ package com.tom;
 
 public class Main {
 	public static void main(String[] args) {
-		Queue<Integer> queue = new Queue<>();
-		queue.enQueue(11);
-		queue.enQueue(22);
-		queue.enQueue(33);
-		queue.enQueue(44);
+//		Queue<Integer> queue = new Queue<>();
+//		queue.enQueue(11);
+//		queue.enQueue(22);
+//		queue.enQueue(33);
+//		queue.enQueue(44);
+//		
+//		System.out.println(queue.size());
+//		while (!queue.isEmpty()) {
+//			System.out.println(queue.deQueue());
+//		}
 		
-		System.out.println(queue.size());
+		Deque<Integer> queue = new Deque<>();
+		queue.enQueueFront(11);
+		queue.enQueueFront(22);
+		queue.enQueueRear(33);
+		queue.enQueueRear(44);
+		/* 44 33 11 22  */
 		while (!queue.isEmpty()) {
-			System.out.println(queue.deQueue());
+			System.out.println(queue.deQueueRear());
+			System.out.println("-----------------------");
 		}
 	}
 }
